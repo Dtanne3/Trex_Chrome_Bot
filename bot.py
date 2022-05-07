d39a163abdc93f3b5b487f6b04959627e20242f1
Drake Tanner <Drizzytanner@gmail.com>
2022-05-07 04:26:56 -0400

    removed actions.py, added to object-recognition

--- bot.py
+++ bot.py
@@ -2,28 +2,50 @@ from cv2 import COLOR_BAYER_BG2GRAY
 import keyboard
 import pyautogui
 import cv2
-import numpy as np
 import time
 
-x, y, w, h = 661, 318, 194, 63
-cactus = cv2.imread("imgs/Cactus4.png")
-temp_gray = cv2.cvtColor(cactus, cv2.COLOR_RGB2GRAY)
-tempWidth, tempHeight = temp_gray.shape[::-1]
+x, y, w, h = 661, 318, 194, 63 #Camera dimensions
+objects = ["imgs/Cactus5.png", "imgs/Char1.png", "imgs/Cactus6.png", "imgs/Cactus7.png"]
+player = cv2.imread("imgs/Player.png")
 
+def getDistance(img):
+    pass
+
+#action inputs
+def jump():
+    keyboard.press('space')
+    time.sleep(0.2)
+    keyboard.release('space')
+
+def crouch(seconds):
+    keyboard.press("down")
+    time.sleep(seconds)
+    keyboard.release("down")
+
+#Execution
 def Run():
     while not keyboard.is_pressed('esc'):
         pyautogui.screenshot("imgs/screen.png", (x,y,w,h))
-        image = cv2.imread("imgs/screen.png")
-        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        matchResult = cv2.matchTemplate (
+        image_gray = cv2.cvtColor(cv2.imread("imgs/screen.png"), cv2.COLOR_BGR2GRAY)
+        
+        for img_input in objects:
+            object = cv2.imread(img_input)
+            temp_gray = cv2.cvtColor(object, cv2.COLOR_RGB2GRAY)
+            #tempWidth, tempHeight = temp_gray.shape[::-1] --will be used later
+            matchResult = cv2.matchTemplate (
                 image = image_gray,
                 templ = temp_gray,
                 method = cv2.TM_CCOEFF_NORMED
             )
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matchResult)
-        if max_val >= 0.3:
-            keyboard.press('space')
-            time.sleep(0.3)
-            keyboard.release('space')
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matchResult)
+            if max_val >= 0.7:
+                #differentiate between cactus and pterodactyl
+                if img_input == "imgs/Char1.png" and max_loc[1] >= 336:
+                    crouch(0.6)
+                else:
+                    jump()
+                    time.sleep(0.1)  #fast-landing strategy until proper prediction method is implemented
+                    crouch(0.2)
+        
                 
             
